4ef39b98ed34d40e5da3c58e1071754efa47c382
tanban <banerjeetuhin801@gmail.com>
2017-12-02 20:09:22 +0530

    my website
    .....

---
+++ index.php
@@ -0,0 +1,64 @@
+<!doctype html>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Sorry Dayeta</title>
+		<link href="about.css" rel="stylesheet" type="text/css" />
+		<link href='https://fonts.googleapis.com/css?family=Calligraffitti' rel='stylesheet' type='text/css'>
+		
+	</head>
+	
+	<body>
+		
+		<div class="flex_container" id="container">
+		
+			
+			
+			<ul id="nav">
+				<li id="link1"><a href="#" target="_self">Sorry</a></li>
+				<li id="link2"><a href="#about" target="_self">sorry</a></li>
+				<li id="link3"><a href="#">sorry</a></li>
+				<li id="link4"><a href="#">sorry</a></li>
+				
+			</ul>
+			
+			<div id="content2">
+				<h2 class="head2">Get Well Soon</h2>
+				<p class="para" id="tuhin"><img src="d1.jpg" style=" margin:0 0 10px 10px; float:right"  width = "250" height="300" alt="here we go">I whole heartedly pray that you get well soon. I am extremely sorry that I forgot to ask about your health today. I was a bit busy. I had a presentation 
+				with my professor. But still I asmit my fault and beg perdon for this. I believe your excellence that you will forgive me<br><br>
+					
+					<br><br>
+					
+				
+				</p>
+				
+				<h2 class="head2" style="position:relative; left:40%">Try to understand !!!</h2>
+				<p class="para" id="tan"><img src="d2.jpg" style=" margin:0 0 10px 10px; float:left; padding-right:40px" width = "250" height="300" alt="here we go">
+					Even if we donot talk everyday everytime, it does not imply that I do not care you. Here is few points for you to appreciate:-
+					<ul style="color:green">
+					<li>You are looking damn cute in this picture</li>
+					<li>Can you please reduce your sentimental mentality!!....believe me it's dreadly!!!</li>
+					<li>Your songs are really awesome and I do like your songs. Sometimes try some bengali modern songs as well !!!</li>
+					<li> I now know that you love to quarrel and believe me I am always there to inspire you in quarreling. </li>
+					<li> Go and study !!!...It's exam time !!!</li>
+					</ul> <br><br>
+				</p>
+				
+				
+				
+				<h2 class="head2">Special note</h2>
+				
+				<p class="para" style="color:red">
+					
+					That's all i can do in this short span of time. I am sorry again if you do not feel it right <br>.....sorrryrrrrrryyy...to be on the safe side :P !!
+				</p>
+				
+				
+				
+					
+			</div>		
+			
+		</div>
+		
+	</body>
